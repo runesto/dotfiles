b06bbabb294a2d5b86a3e12773107131505f88bd
Rune Storløpa <rst@knowit.no>
2019-10-26 17:40:02 +0200

    Fixed installation of mac apps

--- init/40_macos_mac_applicaton_store.sh
+++ init/40_macos_mac_applicaton_store.sh
@@ -12,14 +12,24 @@ function mac_application_store_install() {
   fi
 }
 
-# Exit if Homebrew is not installed.
+function mac_application_store_check_account() {
+  info=$(mas account)
+  if [[ $info == *"Not signed in"* ]]; then
+    e_error "You're not logged in to Mac Application Store. Please log in via GUI."
+    return 1
+  fi
+  e_header "Installing mac apps from account: ${info}"
+}
+
+# Exit if Mas is not installed.
 [[ ! "$(type -P mas)" ]] && e_error "Mac apps need mas to install." && return 1
 
-# Homebrew apps
+# Mac Appstore apps
 apps=(
-  937984704 Amphetamine
+  937984704 #Amphetamine
 )
 
+mac_application_store_check_account
 mac_application_store_install
 
 # Misc cleanup!
