da1682d44620209e74a0115c9075bc2df8183035
Rune Storløpa <rst@knowit.no>
2019-10-27 18:47:32 +0100

    Added personal apps with -p flag

---
+++ init/31_macos_homebrew_casks_personal.sh
@@ -0,0 +1,31 @@
+# macOS-only stuff. Abort if not macOS.
+is_macos || return 1
+
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
+
+# Exit if not run with personal flag
+[[ ! "$include_personal_applications" = true ]] && e_header "Not run with -p or -P. Skipping personal apps" && return 1
+e_header "Installing personal apps"
+
+# Hack to show the first-run brew-cask password prompt immediately.
+brew cask info this-is-somewhat-annoying 2>/dev/null
+
+# Homebrew casks
+casks=(
+  # Applications
+  flowsync
+  logitech-myharmony
+  sonos
+  steam
+)
+
+# Install Homebrew casks.
+casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+if (( ${#casks[@]} > 0 )); then
+  e_header "Installing Homebrew casks: ${casks[*]}"
+  for cask in "${casks[@]}"; do
+    brew cask install $cask
+  done
+  brew cleanup
+fi
