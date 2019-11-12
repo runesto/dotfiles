2c7b2db63ca0e99bd06610e81a648a95bc57d570
Rune Storløpa <EMAIL>
2019-11-12 22:41:55 +0100

    Bugfix

--- init/05_ssh_private_keys.sh
+++ init/05_ssh_private_keys.sh
@@ -1,2 +1,2 @@
-# Add all private keys in ~/.ssh/ to Keychain
-find -L ~/.ssh/keys -type f ! -name "*.pub" | xargs ssh-add -K
+# Add all private keys in ~/.ssh/keys to Keychain
+find -L ~/.ssh/keys -type f ! -name "*.pub" ! -name ".gitkeep" | xargs ssh-add -K
