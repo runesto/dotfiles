d12c17933d7a3790d057cf3f9db1974277f68334
Rune Storløpa <EMAIL>
2019-11-16 20:35:15 +0100

    Changed tests

--- .github/workflows/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#check personal installation
-[[ ! -d  "/Applications/Steam.app" ]] && exit 1
-#check alias
-! alias ll 2>/dev/null >/dev/null && exit 1
-# verify that it's running
-cd "/Applications/Steam.app"
-ll
