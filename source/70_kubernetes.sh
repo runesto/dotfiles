0cc84ec03230e3889a15aec06e6d0cbcab89fa8a
Rune Storløpa <runesto@gmail.com>
2020-02-25 14:18:22 +0100

    Bugfix

--- source/70_kubernetes.sh
+++ source/70_kubernetes.sh
@@ -1,3 +1,3 @@
 kdel () {
-    kubectl -n $1 get pods | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} $2 | cut -f 1 -d ” ” | xargs -n1 kubectl -n $1 delete pod
+    kubectl -n $1 get pods | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} $2 | cut -f 1 -d ' ' | xargs -n1 kubectl -n $1 delete pod
 }
