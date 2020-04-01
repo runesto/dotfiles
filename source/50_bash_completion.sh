e614e8a1c56cc63c8068692244d56a6ca9681f6e
Rune Storløpa <runesto@gmail.com>
2020-04-01 12:41:42 +0200

    Added docker-completion

--- source/50_bash_completion.sh
+++ source/50_bash_completion.sh
@@ -1,5 +1,10 @@
 # Exit if Homebrew is not installed
 [[ ! "$(type -P brew)" ]] && e_error "Bash_completion expects Homebrew to be installed." && return 1
 
+# Link Docker completion (requires Docker installed)
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker.bash-completion /usr/local/etc/bash_completion.d/docker
+ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion /usr/local/etc/bash_completion.d/docker-compose
+
+
 # Source bash_completion
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
