d7452b056656732e0e25ce2080b04bf17176e424
Rune Storløpa <runesto@gmail.com>
2020-05-19 13:40:51 +0200

    Adding to path because of warning when running homebrew

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,1 +1,6 @@
 export PATH="$DOTFILES/bin:$HOME/.bin:$PATH"
+
+#Warning: Homebrew's sbin was not found in your PATH but you have installed
+#formulae that put executables in /usr/local/sbin.
+#Consider setting the PATH for example like so:
+export PATH="/usr/local/sbin:$PATH"
