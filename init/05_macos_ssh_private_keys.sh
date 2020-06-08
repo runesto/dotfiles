b523093b549e66e68ec8d6ea76418a16d139f688
Rune Storløpa <rune.storloepa@sparebank1.no>
2020-06-08 19:06:54 +0200

    Different ssh config for macos and rhel

--- init/05_macos_ssh_private_keys.sh
+++ init/05_macos_ssh_private_keys.sh
@@ -1,5 +1,8 @@
 # macOS-only stuff. Abort if not macOS.
 is_macos || return 1
 
+# Use config file via symlinks
+ln -sf ~/.ssh/config_macos ~/.ssh/config
+
 # Add all private keys in ~/.ssh/keys to Keychain
 find -L ~/.ssh/keys -type f ! -name "*.pub" ! -name ".gitkeep" | xargs ssh-add -K
