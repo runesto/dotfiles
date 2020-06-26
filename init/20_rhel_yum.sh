ffa541dd8be1280a8e42c20e4daa841811d66451
Rune Storløpa <rune.storloepa@sparebank1.no>
2020-06-26 20:31:17 +0200

    More effective yum handling

--- init/20_rhel_yum.sh
+++ init/20_rhel_yum.sh
@@ -1,19 +1,29 @@
-# Ubuntu-only stuff. Abort if not Ubuntu.
+# RHEL-only stuff. Abort if not RHEL.
 is_rhel || return 1
+
+installed_yum_packages=()
+yum_packages=()
+yum_packages_to_install=()
 
 # Misc.
 yum_packages+=(
   atom
-  blueberry
+#  blueberry
   ImageMagick
-  redshift
+  redshift # mangler config
   teams
+  tree
+  tuned-profiles-compat # tuned-adm profile desktop-powersave
 )
 
-if (( ${#yum_packages[@]} > 0 )); then
-  e_header "Installing yum packages (${#yum_packages[@]})"
-  for package in "${yum_packages[@]}"; do
+# Install Yum packages.
+installed_yum_packages="$(yum list installed -q | grep -v 'Installed Packages' | grep -v 'Loaded plugins' | cut -d ' ' -f 1 | cut -d '.' -f 1 | uniq)"
+yum_packages_to_install=($(setdiff "${yum_packages[*]}" "$installed_yum_packages"))
+
+if (( ${#yum_packages_to_install[@]} > 0 )); then
+  e_header "Installing yum packages (${#yum_packages_to_install[@]})"
+  for package in "${yum_packages_to_install[@]}"; do
     e_arrow "$package"
-    sudo yum install -q "$package"
+    sudo yum install -q -y "$package"
   done
 fi
