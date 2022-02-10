33836c9d3a9f060b981c4471b081eb03ed18831b
Rune Storløpa <>
2022-02-10 14:55:14 +0100

    Better naming

--- source/50_docker.sh
+++ source/50_docker.sh
@@ -1,12 +1,12 @@
 # Not RHEL-tested. Abort if RHEL
 is_rhel && return 1
 
-# Remove all docker containers and images
+# Remove all docker containers and images and volumes
 function dockerclean () { (
 	docker rm -f $(docker ps -a -q) || true) && (docker rmi -f $(docker images -q) || true) && (docker volume rm $(docker volume ls |awk '{print $2}') || true) ;
 }
 
 # Stop all docker containers
-function dockerstopall () { 
+function dockerdeleteallcontainers () {
 	(docker rm -f $(docker ps -a -q)) 
 } 
