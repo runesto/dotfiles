fab42dcb66bc63149811785c87240c66afdfd722
Rune Storløpa <>
2022-03-16 15:27:42 +0100

    Fixed local branch pruning command

--- source/50_git.sh
+++ source/50_git.sh
@@ -1,4 +1,4 @@
 
 gitprunebranches () {
-	git branch --v | grep "\[gone\]" | awk '{print $1}' | xargs git branch -D
+	git remote prune origin; git branch --v | grep "\[gone\]" | awk '{print $1}' | xargs git branch -D
 }
