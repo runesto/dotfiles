c157f5f4546cb75281fdf4df674f942f9900cbcf
Rune Storløpa <runesto@users.noreply.github.com>
2023-03-24 13:24:08 +0100

    Java version shortcuts

--- source/40_java.sh
+++ source/40_java.sh
@@ -1,2 +1,10 @@
 # For maven to work
 export JAVA_HOME=$(/usr/libexec/java_home)
+
+function java8 () {
+	export JAVA_HOME=$(/usr/libexec/java_home -v 1.8);
+}
+
+function java17 () {
+	export JAVA_HOME=$(/usr/libexec/java_home -v 17);
+}
