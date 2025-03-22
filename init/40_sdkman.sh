3b420b005dbad6d663b6ff4c47c4c639a3744d5c
Rune Storløpa <>
2025-03-22 12:45:23 +0100

    Fix check

--- init/40_sdkman.sh
+++ init/40_sdkman.sh
@@ -2,7 +2,8 @@
 is_macos || return 1
 
 # Install SDKMAN
-if [[ ! -d "$HOME/.sdk" ]]; then
+if [[ ! -d "$HOME/.sdkman" ]]; then
   e_header "Installing SDKMAN"
   true | /bin/bash -c "$(curl -fsSL https://get.sdkman.io)"
+  echo "DOTFILES NOTE: Added lines to .zshrc and .bash_profile can be deleted. Handled in existing file"
 fi
