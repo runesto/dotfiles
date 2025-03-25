36b214c7807ba4428158ba55dd258efc639bd80d
Rune Storløpa <>
2025-03-25 15:58:55 +0100

    Nebula-tools på skatteetaten

---
+++ source/98_skatt_nebula.sh
@@ -0,0 +1,8 @@
+if [[ ! -d "$HOME/src/skatt/neb/nebula-tools" ]]; then
+  e_header "Installing Nebula tools"
+  md ${HOME}/src/skatt/neb/
+  git clone https://git.aurora.skead.no/scm/neb/nebula-tools.git
+  cd
+fi
+
+export PATH=$PATH:${HOME}/src/skatt/neb/nebula-tools/scripts
